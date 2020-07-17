70c38eaa2f3cec40f77205958cd9467ab96b6f0f
Vladyslav2020 <68378725+Vladyslav2020@users.noreply.github.com>
2020-07-17 13:30:04 +0300

    Update main_script.js

--- main_script.js
+++ main_script.js
@@ -9,12 +9,12 @@ input.addEventListener('keypress', function (e){
 function add_block_item(){
 	let div = document.createElement('div'),
 		inp = document.querySelector('.todos-input input');
-	div.innerHTML = '<div class = "checker" onclick = "do_complete(this)"></div><div class = "remove" onclick = "remove(this)">X</div>' + inp.value;
+	div.innerHTML = '<div class = "checker" onclick = "do_complete(this)"></div><div class = "remove" onclick = "remove(this)"><i class="fa fa-times" aria-hidden="true"></i></div>' + inp.value;
 	div.classList.add('item');
 	inp.value = '';
 		
 	document.querySelector(".todos .todos-items").appendChild(div);
-	cntIncrement();
+	reCalc();
 	let buttons = document.querySelectorAll('.todos-footer .button');
 	if (buttons[1].classList.contains('active'))
 		show_active();
@@ -23,22 +23,19 @@ function add_block_item(){
 		show_complete();
 }
 
-function cntIncrement(){
-	let cnt = document.querySelector(".todos-footer .cnt-items span");
-	cnt.innerHTML = +cnt.innerText + 1;
-}
-
-function cntDecrement(){
-	let cnt = document.querySelector(".todos-footer .cnt-items span");
-	cnt.innerHTML = +cnt.innerText - 1;
+function reCalc(){
+	let cnt = document.querySelector(".todos-footer .cnt-items span"),
+		count = 0,
+		elems = document.querySelectorAll('.todos .item .checker');
+	for (let item of elems)
+		if (!item.classList.contains('complete'))
+			count++;
+	cnt.innerHTML = count;
 }
 
 function do_complete(el){
 	el.classList.toggle('complete');
-	if (el.classList.contains('complete'))
-		cntDecrement();
-	else
-		cntIncrement();
+	reCalc();
 	let buttons = document.querySelectorAll('.todos-footer .button');
 	if (buttons[1].classList.contains('active'))
 		show_active();
@@ -93,5 +90,25 @@ function clear_complete(){
 function remove(elem){
 	elem.parentElement.remove();
 	if (!elem.parentElement.children[0].classList.contains('complete'))
-		cntDecrement();
+		reCalc();
 }
+
+function completed_all(){
+	let elem = document.querySelector('.todos .todos-input i');
+	elem.classList.toggle('active');
+	let elems = document.querySelectorAll('.todos .item');
+	if (elem.classList.contains('active')){
+		elems.forEach(item => item.children[0].classList.add('complete'));
+	}
+	else{
+		elems.forEach(item => item.children[0].classList.remove('complete'));
+	}
+	reCalc();
+	let buttons = document.querySelectorAll('.todos-footer .button');
+	if (buttons[1].classList.contains('active'))
+		show_active();
+	else
+	if (buttons[2].classList.contains('active'))
+		show_complete();
+}
+	
